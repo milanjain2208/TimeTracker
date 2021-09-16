47e70ff9df5e760b25e93e438547673061b2d3f7
Milan Jain <milanj@adobe.com>
2021-09-17 03:12:48 +0530

    html and css 1st iter

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    const workCurrent = document.querySelector(".work .current");
+    const workPrevious = document.querySelector(".work .previous");
+    const playCurrent = document.querySelector(".play .current");
+    const playPrevious = document.querySelector(".play .previous");
+    const studyCurrent = document.querySelector(".study .current");
+    const studyPrevious = document.querySelector(".study .previous");
+    const exerciseCurrent = document.querySelector(".exercise .current");
+    const exercisePrevious = document.querySelector(".exercise .previous");
+    const socialCurrent = document.querySelector(".social .current");
+    const socialPrevious = document.querySelector(".social .previous");
+    const selfcareCurrent = document.querySelector(".selfcare .current");
+    const selfcarePrevious = document.querySelector(".selfcare .previous");
+}
